a965a30eb53c1796cd9cf466f456241a9643c3f4
Pix <jens@Jens-iMac.local>
2016-04-26 15:47:54 +0200

    Adapter created

---
+++ README.md
@@ -0,0 +1,49 @@
+![Logo](admin/feiertage.png)
+# ioBroker.feiertage
+=================
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.feiertage.svg)](https://www.npmjs.com/package/iobroker.feiertage)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.feiertage.svg)](https://www.npmjs.com/package/iobroker.feiertage)
+[![Tests](https://travis-ci.org/ioBroker/ioBroker.feiertage.svg?branch=master)](https://travis-ci.org/ioBroker/ioBroker.feiertage)
+
+[![NPM](https://nodei.co/npm/iobroker.feiertage.png?downloads=true)](https://nodei.co/npm/iobroker.feiertage/)
+
+Dieser Adapter liefert das Datum und den Namen des nächsten deutschen Feiertages und gibt Auskunft, ob heute, morgen oder übermorgen ein Feiertag ist.
+
+Es können keine Einstellungen vorgenommen werden.
+
+Der Adapter startet jeden Tag um Mitternacht. Ein häufigeres Starten ist nicht erforderlich.
+
+
+
+## Changelog
+### 0.0.1 (2016-04-26)
+* (pix) Adapter created
+
+## Todo
+
+* Übersetzungen
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2016 pix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ feiertage.js
@@ -0,0 +1,133 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+
+"use strict";
+
+var utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
+var tools   = require(__dirname + '/lib/tools');
+
+var channels = [];
+var iopkg;
+
+var adapter = utils.adapter({
+    name: 'feiertage',
+    useFormatDate: true
+});
+
+adapter.on('ready', checkHolidays);
+
+
+setTimeout(function () {
+    adapter.log.info('force terminating after 4 minutes');
+    adapter.stop();
+}, 240000);
+
+
+var idheute =           "heute.boolean",
+    idmorgen =          "morgen.boolean",
+    iduebermorgen =     "uebermorgen.boolean",
+    idheuteName =       "heute.Name",
+    idmorgenName =      "morgen.Name",
+    iduebermorgenName = "uebermorgen.Name",
+    idnaechsterDatum =  "naechster.Datum",
+    idnaechsterName =   "naechster.Name",
+    idnaechsterDauer =  "naechster.Dauer";
+
+
+// Script ermittelt, ob heute oder morgen ein Feiertag ist
+var sj;                 // Schaltjahr
+var ostern;             // Ostersonntag: Tag im Jahr
+
+// Regional nicht relevante Feiertage auskommentieren oder löschen !!
+function feiertag(day) {
+    var Name = '';
+    if (day == 1) Name = 'Neujahr';
+    else if (day == 6) Name = 'Heilige Drei Könige (BW,BY,ST)';
+    else if (sj + 121 == day) Name = 'Maifeiertag';
+    else if (day == sj + 227) Name = 'Maria Himmelfahrt (BY (nicht überall), SL)';
+    else if (day == sj + 276) Name = 'Tag der dt. Einheit';
+    else if (day == sj + 304) Name = 'Reformationstag (BB, MV, SA, ST,TH)';
+    else if (day == sj + 305) Name = 'Allerheiligen (BW, BY, NW, RP, SL)';
+    else if (day == sj + 358) Name = 'Heiligabend';
+    else if (day == sj + 359) Name = '1. Weihnachtstag';
+    else if (day == sj + 360) Name = '2. Weihnachtstag';
+    else if (day == sj + 365) Name = 'Silvester';
+     
+    else if (day == ostern - 48) Name = 'Rosenmontag';
+    else if (day == ostern -  2) Name = 'Karfreitag';
+    else if (day == ostern)      Name = 'Ostersonntag';
+    else if (day == ostern +  1) Name = 'Ostermontag';
+    else if (day == ostern + 39) Name = 'Christi Himmelfahrt';
+    else if (day == ostern + 49) Name = 'Pfingstsonntag';
+    else if (day == ostern + 50) Name = 'Pfingstmontag';
+    else if (day == ostern + 60) Name = 'Fronleichnam (BW, BY, HE, NW, RP, SL (SA, TH nicht überall))';
+    else Name = '';
+    return (Name);
+    
+} 
+
+function tagdesjahresZudatum (day) {
+    var day_ms = (day - sj) * 24 * 60 * 60 * 1000;                              // Tag des Jahres in Millisekunden seit Neujahr Mitternacht
+    var jetzt = new Date();
+    var Jahr = jetzt.getFullYear();
+    var neujahr = new Date(Jahr,0,1,0,0,0,0);                                   // Dies Jahr Neujahr Mitternacht
+    var neujahr_ms = neujahr.getTime();
+    
+    var datum = new Date();
+    datum.setTime(neujahr_ms + day_ms);                                         // Addition Neujahr in ms plus Tag des Jahres in ms = gesuchtes Datum in ms
+    return(datum);    
+}
+
+
+// auf Feiertag testen
+function checkHolidays() {
+   var jetzt = new Date();
+   var Jahr = jetzt.getFullYear();
+   sj = (Jahr % 4 === 0) ? 1 : 0;
+
+// Die modifizierte Gauss-Formel nach Lichtenberg, gültig bis 2048
+    var A = 120 + (19 * (Jahr % 19) + 24) % 30;
+    var B = (A + parseInt(5 * Jahr / 4)) % 7;
+    ostern = A - B - 33 + sj;
+
+// Tag des Jahres
+   var heutestart = new Date(jetzt.setHours(0,0,0,0));
+   var neujahr = new Date(Jahr,0,1);
+   var difftage = (heutestart - neujahr) / (24*60*60*1000) + 1;
+   var tag = Math.ceil(difftage);
+
+   // heute 
+   var istFeiertag;
+   adapter.setState(idheuteName, feiertag(tag));
+   istFeiertag = (feiertag(tag).length < 2) ? false : true;
+   adapter.setState(idheute, istFeiertag);
+   
+   // morgen
+   tag = tag + 1;
+   if (tag > 365 + sj) tag = 1;
+   adapter.setState(idmorgenName, feiertag(tag));
+   istFeiertag = (feiertag(tag).length < 2) ? false : true;
+   adapter.setState(idmorgen, istFeiertag);
+   
+   // übermorgen
+   tag = tag + 1;
+   if (tag > 365 + sj) tag = 1;
+   adapter.setState(iduebermorgenName, feiertag(tag));
+   istFeiertag = (feiertag(tag).length < 2) ? false : true;
+   adapter.setState(iduebermorgen, istFeiertag);
+   
+   // nächster Feiertag
+   var noch = 0;
+   while (feiertag(tag).length < 2) {
+       tag = tag + 1;
+       noch = noch + 1;
+       istFeiertag = (feiertag(tag).length < 2) ? false : true;
+       if (istFeiertag) {
+           var datum_tdj = tagdesjahresZudatum(tag);
+           adapter.setState(idnaechsterName, feiertag(tag));
+           adapter.setState(idnaechsterDatum, formatDate(datum_tdj));
+           adapter.log.info('Nächster Feiertag: '  + feiertag(tag) + ' in ' + noch + ' Tagen am ' + formatDate(datum_tdj));
+           adapter.setState(idnaechsterDauer, noch);
+       }
+   }
+}
