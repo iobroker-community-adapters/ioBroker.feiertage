9dabb78fc2e62bde48953447c27dcb087ac75c33
Pix <Pix---@users.noreply.github.com>
2016-05-08 00:41:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 /*jslint node: true */
 
 "use strict";
-var utils    = require(__dirname + '/lib/utils'); // Get common adapter utils
-var holidays = require(__dirname + '/admin/holidays').holidays; // Get common adapter utils
+var utils       = require(__dirname + '/lib/utils'); // Get common adapter utils
+var parseString = require('xml2js').parseString;
+var request     = require('request');
 
 var lang = 'de';
 
 var adapter = utils.adapter({
-    name:           'feiertage',
+    name:           'tvspielfilm',
     useFormatDate:  true
 });
 
@@ -18,9 +19,9 @@ adapter.on('ready', function () {
             lang  = data.common.language;
         }
 
-        adapter.log.debug('adapter feiertage initializing objects');
-        checkHolidays();
-        adapter.log.info('adapter feiertage objects written');
+        adapter.log.debug('adapter tvspielfilm initializing objects');
+        main();
+        adapter.log.info('adapter tvspielfilm objects written');
 
         setTimeout(function () {
             adapter.log.info('force terminating after 1 minute');
@@ -31,109 +32,104 @@ adapter.on('ready', function () {
 });
  
 function readSettings() {
-    for (var h in holidays) {
-        holidays[h].enabled = adapter.config['enable_' + h];
-    }
-} 
-
-// Get the name of holiday for the day of the year
-function getHoliday(day, isLeap, easter, _lang) {
-    _lang = _lang || lang;
-
-    for (var h in holidays) {
-        if (holidays[h].enabled) {
-            if (holidays[h].offset && (holidays[h].offset + (holidays[h].leap ? isLeap : 0)) == day) {
-                return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
-            }
-            if (holidays[h].easterOffset && (easter + holidays[h].easterOffset) == day) {
-                return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
-            }
+    var path = __dirname;
+        /*if (typeof blacklist == 'function') {
+            callback = blacklist;
+            blacklist = undefined;
         }
-    }
-    return '';
+    adapter.log.info('Blacklist: ' + blacklist);*/
+    var blacklist = adapter.config.stations;
+    adapter.log.info('Stationen aus Blacklist: ' + blacklist);
+    
 } 
 
-function getDateFromYearsDay(day, isLeap) {
-    var dayMs     = (day - isLeap) * 24 * 60 * 60 * 1000; // Day of the year in ms from 01.01 00:00:00
-    var now       = new Date();
-    var year      = now.getFullYear();
-    var newYear   = new Date(year, 0, 1, 0, 0, 0, 0);     // This year 01.01 00:00:00
-    var newYearMs = newYear.getTime();
-    var date      = new Date();
+function check_sender (ueberschrift) { //  wird so übergeben "16:50 | Sky Cinema | Kill the Boss 2"
+    var ueberschrift_teile = ueberschrift.split(' | ');
+    var sender = ueberschrift_teile[1];
+    var empfangbar;
     
-    date.setTime(newYearMs + dayMs);                      // Add to current New year the ms
-    return date;    
+    /*var blacklist = [];
+    blacklist = ['Silverline',
+                 'Sky Action',
+                 'Sky Cinema',
+                 'Sky Comedy',
+                 'MGM HD',
+                 'Sky Nostalgie',
+                 'KinoweltTV',
+                 'Sky Emotion',
+                 'Sky 007 HD',
+                 'Disney Channel',
+                 'Sony E. Television',
+                 'TNT Film',
+                 'Sky Hits HD',
+                 'Disney Cinemagic',
+                 'AXN',
+                 'Syfy',
+                 'Sky Atlantic HD'
+                ];
+    var suchergebnis = blacklist.indexOf(sender,0);  // Ergebnis ist die Position im Array oder "-1", wenn nicht gefunden
+    empfangbar = (suchergebnis == -1) ? true : false; // Sender nicht in der Blacklist, also empfangbar*/
+    return(true);//return(empfangbar); 
 }
 
-// check the holidays
-function checkHolidays() {
-    readSettings();
-    var now    = new Date();
-    var year   = now.getFullYear();
-    var isLeap = (year % 4 === 0) ? 1 : 0;
 
-    // THe modified Gauss-Formula to calculate catholic eastern, valid till 2048
-    var A = 120 + (19 * (year % 19) + 24) % 30;
-    var B = (A + parseInt(5 * year / 4)) % 7;
-    // Easter sunday: day of the year
-    var easter = A - B - 33 + isLeap;
+var rss_options = {
+    jetzt : { feedname: 'Jetzt',
+              url: 'http://www.tvspielfilm.de/tv-programm/rss/jetzt.xml',
+              state: 'rss.jetzt',
+              cssclass:  'tv_jetzt'
+            },
+    tipps:  { feedname: 'Tipps',
+              url: 'http://www.tvspielfilm.de/tv-programm/rss/filme.xml',
+              state: 'rss.tipps',
+              cssclass:  'tv_tipps'
+            }
+}
 
-    // Day of the year
-    var todayStart = new Date(now.setHours(0, 0, 0, 0));
-    var newYear    = new Date(year, 0, 1);
-    var diffDays   = (todayStart - newYear) / (24 * 60 * 60 * 1000) + 1;
-    var day        = Math.ceil(diffDays);
 
-    // today
-    var hd = getHoliday(day, isLeap, easter);
-    adapter.setState('heute.Name',    {ack: true, val: getHoliday(day, isLeap, easter, 'de')});
-    adapter.setState('heute.boolean', {ack: true, val: !!hd});
-    adapter.setState('today.name',    {ack: true, val: hd});
-    adapter.setState('today.boolean', {ack: true, val: !!hd});
-
-    // tomorrow
-    day = day + 1;
-    if (day > 365 + isLeap) day = 1;
-    hd = getHoliday(day, easter);
-    adapter.setState('morgen.Name',      {ack: true, val: getHoliday(day, isLeap, easter, 'de')});
-    adapter.setState('morgen.boolean',   {ack: true, val: !!hd});
-    adapter.setState('tomorrow.name',    {ack: true, val: hd});
-    adapter.setState('tomorrow.boolean', {ack: true, val: !!hd});
-
-    // the day after tomorrow
-    day = day + 1;
-    if (day > 365 + isLeap) day = 1;
-    hd = getHoliday(day, isLeap, easter);
-    adapter.setState('uebermorgen.Name',      {ack: true, val: getHoliday(day, isLeap, easter, 'de')});
-    adapter.setState('uebermorgen.boolean',   {ack: true, val: !!hd});
-    adapter.setState('aftertomorrow.name',    {ack: true, val: hd});
-    adapter.setState('aftertomorrow.boolean', {ack: true, val: !!hd});
-
-    // next holiday
-    var duration = 0;
-    day = day - 1; // shift back to "tomorrow", because tomorrow is the next day
-    do {
-        day = day + 1;
-        if (day > 365 + isLeap) day = 1;
-        duration = duration + 1;
-        hd = getHoliday(day, isLeap, easter);
-
-        if (hd) {
-            var date = getDateFromYearsDay(day, isLeap);
-            adapter.setState('naechster.Name', {ack: true, val: getHoliday(day, isLeap, easter, 'de')});
-            adapter.setState('next.name',      {ack: true, val: hd});
-
-            var nextHoliday = adapter.formatDate(date);
-            adapter.log.info('Next holiday: ' + hd + ' is in ' + duration + ' days on ' + nextHoliday);
-
-            adapter.setState('naechster.Datum', nextHoliday, true);
-            adapter.setState('next.date',       nextHoliday, true);
-
-            adapter.setState('naechster.Dauer', duration, true);
-            adapter.setState('next.duration',   duration, true, function () {
-                adapter.stop();
+function readFeed (x) {
+    var link = rss_options[x].url;
+    adapter.log.info('RSS Feed wird eingelesen: ' + link);
+    request(link, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+    
+            parseString(body, {
+                explicitArray: false,
+                mergeAttrs: true
+            }, function (err, result) {
+                var data = JSON.stringify(result, null, 2);
+                var table = [];
+                if (err) {
+                    adapter.log.warn("Fehler: " + err);
+                } else {                                                               
+                    var sender_empfangbar = false;
+                    if (result.rss.channel.item.length !== null) { // gelegentlicher Fehler bei nächtlicher Abfrage durch length (undefined) soll hier abgefangen werden
+                        // Array durchzaehlen von 0 bis Zahl der items
+                        for(var i = 0; i < result.rss.channel.item.length; i++) {
+                            sender_empfangbar = check_sender(result.rss.channel.item[i].title);
+                            if (sender_empfangbar) {
+                                var entry = {
+                                    image: result.rss.channel.item[i].enclosure ? '<img width="100%" src="' + result.rss.channel.item[i].enclosure.url + '" />' : '',
+                                    text:  '<table class="' + rss_options[x].cssclass + '"><tr><td class="' + rss_options[x].cssclass + '_text" style="text-align: left; padding-left: 5px; font-weight: bold"><a href="' +
+                                       result.rss.channel.item[i].link + '" target="_blank">' + result.rss.channel.item[i].title +
+                                       '</a></td></tr><tr><td style="text-align: left; padding-left: 5px">' +
+                                       result.rss.channel.item[i].description +'</td></tr></table>',
+                                    _Bild: result.rss.channel.item[i].enclosure ? '<img class="' + rss_options[x].cssclass + '_bild" width="100%" src="' + result.rss.channel.item[i].enclosure.url + '" />' : 'no image'
+                                };
+                                table.push(entry);
+                            } // Ende Abfrage, ob Sender empfangbar
+                        }
+                    } else adapter.log.warn('LENGTH in TV Programm (' + rss_options[x].feedname + ') nicht definiert'); // ende if ungleich
+                }
+                adapter.setState(rss_options[x].state, {val: JSON.stringify(table), ack: true});// ganze XML in Objekt für Table Widget
             });
-            break;
-        }
-    } while (!hd);
+        } else adapter.log.warn(error,'error');
+    });   // Ende request 
+    adapter.log.info('XML-Daten aus TV Spielfilm (' + rss_options[x].feedname + ') eingelesen');
+}
+function main() {
+    for (var j in rss_options) {
+        readFeed(j);
+    }
+    adapter.stop();
 }
