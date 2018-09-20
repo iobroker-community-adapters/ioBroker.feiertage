8cccf42e0ff1908c1e20164bedb0e94aac195787
Pix <Pix---@users.noreply.github.com>
2018-09-20 09:53:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,38 +2,20 @@
 /*jslint node: true */
 
 "use strict";
-var utils    = require(__dirname + '/lib/utils'); // Get common adapter utils
-var holidays = require(__dirname + '/admin/holidays').holidays; // Get common adapter utils
+var utils    = require(__dirname + "/lib/utils"); // Get common adapter utils
+var holidays = require(__dirname + "/admin/holidays").holidays; // Get common adapter utils
 
-var lang = 'de';
+var lang = "de";
 
 var adapter = utils.Adapter({
-    name:           'feiertage',
+    name:           "feiertage",
     useFormatDate:  true
-});
-
-adapter.on('ready', function () {
-    adapter.getForeignObject('system.config', function (err, data) {
-        if (data && data.common) {
-            lang  = data.common.language;
-        }
-
-        adapter.log.debug('adapter feiertage initializing objects');
-        checkHolidays();
-        adapter.log.info('adapter feiertage objects written');
-
-        setTimeout(function () {
-            adapter.log.info('force terminating after 1 minute');
-            adapter.stop();
-        }, 60000);
-
-    });
 });
  
 function readSettings() {
     var isOneEnabled = false;
     for (var h in holidays) {
-        holidays[h].enabled = (adapter.config['enable_' + h] !== undefined) ? (adapter.config['enable_' + h] === true || adapter.config['enable_' + h] === 'true') : holidays[h].enabled;
+        holidays[h].enabled = (adapter.config["enable_" + h] !== undefined) ? (adapter.config["enable_" + h] === true || adapter.config["enable_" + h] === "true") : holidays[h].enabled;
         if (holidays[h].enabled) isOneEnabled = true;
     }
     return isOneEnabled;
@@ -45,37 +27,37 @@ function getHoliday(day, isLeap, easter, advent4, year, _lang) {
 
     for (var h in holidays) {
         if (holidays[h].enabled) {
-            if (holidays[h].offset !== 'undefined' && (1 + holidays[h].offset + (holidays[h].offset >= 59 ? isLeap : 0)) == day) {
-                return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
+            if (holidays[h].offset !== "undefined" && (1 + holidays[h].offset + (holidays[h].offset >= 59 ? isLeap : 0)) == day) {
+                return holidays[h][_lang] + (holidays[h]["comment_" + _lang] ? " " + holidays[h]["comment_" + _lang] : "");
             }
-            if (holidays[h].easterOffset !== 'undefined' && (easter + holidays[h].easterOffset) == day) {
-                return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
+            if (holidays[h].easterOffset !== "undefined" && (easter + holidays[h].easterOffset) == day) {
+                return holidays[h][_lang] + (holidays[h]["comment_" + _lang] ? " " + holidays[h]["comment_" + _lang] : "");
             }
-            if (holidays[h].advent4Offset !== 'undefined' && (advent4 + holidays[h].advent4Offset) == day) {
-                return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
+            if (holidays[h].advent4Offset !== "undefined" && (advent4 + holidays[h].advent4Offset) == day) {
+                return holidays[h][_lang] + (holidays[h]["comment_" + _lang] ? " " + holidays[h]["comment_" + _lang] : "");
             }
-            if (holidays[h].april30Offset !== 'undefined') {
+            if (holidays[h].april30Offset !== "undefined") {
                 var newYear = new Date(year, 0, 1);
                 var april30date = new Date(year, 3, 30, 0, 0, 0);
                 var april30num = Math.ceil((april30date - newYear) / (24 * 60 * 60 * 1000) + 1);
                 var mday = april30num - april30date.getDay() + holidays[h].april30Offset;
 
                 if (mday == day) {
-                  return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
+                  return holidays[h][_lang] + (holidays[h]["comment_" + _lang] ? " " + holidays[h]["comment_" + _lang] : "");
                 }
             }
-            if (holidays[h].michaelisOffset !== 'undefined') {
+            if (holidays[h].michaelisOffset !== "undefined") {
                 var newYear = new Date(year, 0, 1);
                 var michaelisDate = new Date(year, 8, 29, 0, 0, 0);
                 var michaelisNum = Math.ceil((michaelisDate - newYear) / (24 * 60 * 60 * 1000) + 1);
 
                 if ((michaelisNum + (holidays[h].michaelisOffset - michaelisDate.getDay())) == day) {
-                  return holidays[h][_lang] + (holidays[h]['comment_' + _lang] ? ' ' + holidays[h]['comment_' + _lang] : '');
+                  return holidays[h][_lang] + (holidays[h]["comment_" + _lang] ? " " + holidays[h]["comment_" + _lang] : "");
                 }
             }
         }
     }
-    return '';
+    return "";
 } 
 
 function getDateFromYearsDay(day, year) {
@@ -91,15 +73,15 @@ function getDateFromYearsDay(day, year) {
 // check the holidays
 function checkHolidays() {
     if (!readSettings()) {
-        adapter.log.error('No one holiday is enabled');
+        adapter.log.error("No one holiday is enabled");
         adapter.stop();
         return;
     }
     var now    = new Date();
     var year   = now.getFullYear();
-    var isLeap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? 1 : 0;
+    var isLeap = ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0) ? 1 : 0;
 
-    // THe modified Gauss-Formula to calculate catholic eastern, valid till 2048
+    // The modified Gauss-Formula to calculate catholic eastern, valid till 2048
     var A = 120 + (19 * (year % 19) + 24) % 30;
     var B = (A + parseInt(5 * year / 4)) % 7;
     // Easter sunday: day of the year
@@ -113,33 +95,33 @@ function checkHolidays() {
 
     // 4th advent (sunday before first christmas day)
     var christmas1 = new Date(year, 11, 25, 12, 0, 0);
-    var advent4date = new Date(christmas1.getTime() - (((christmas1.getDay() == 0) ? 7 : christmas1.getDay()) * (24 * 60 * 60 * 1000)));
+    var advent4date = new Date(christmas1.getTime() - (((christmas1.getDay() === 0) ? 7 : christmas1.getDay()) * (24 * 60 * 60 * 1000)));
     var advent4 = Math.ceil((advent4date.setHours(0, 0, 0, 0) - newYear) / (24 * 60 * 60 * 1000) + 1);
 
     // today
     var hd = getHoliday(day, isLeap, easter, advent4, year);
-    adapter.setState('heute.Name',    {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, 'de')});
-    adapter.setState('heute.boolean', {ack: true, val: !!hd});
-    adapter.setState('today.name',    {ack: true, val: hd});
-    adapter.setState('today.boolean', {ack: true, val: !!hd});
+    adapter.setState("heute.Name",    {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, "de")});
+    adapter.setState("heute.boolean", {ack: true, val: !!hd});
+    adapter.setState("today.name",    {ack: true, val: hd});
+    adapter.setState("today.boolean", {ack: true, val: !!hd});
 
     // tomorrow
     day = day + 1;
     if (day > 365 + isLeap) day = 1;
     hd = getHoliday(day, isLeap, easter, advent4, year);
-    adapter.setState('morgen.Name',      {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, 'de')});
-    adapter.setState('morgen.boolean',   {ack: true, val: !!hd});
-    adapter.setState('tomorrow.name',    {ack: true, val: hd});
-    adapter.setState('tomorrow.boolean', {ack: true, val: !!hd});
+    adapter.setState("morgen.Name",      {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, "de")});
+    adapter.setState("morgen.boolean",   {ack: true, val: !!hd});
+    adapter.setState("tomorrow.name",    {ack: true, val: hd});
+    adapter.setState("tomorrow.boolean", {ack: true, val: !!hd});
 
     // the day after tomorrow
     day = day + 1;
     if (day > 365 + isLeap) day = 1;
     hd = getHoliday(day, isLeap, easter, advent4, year);
-    adapter.setState('uebermorgen.Name',      {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, 'de')});
-    adapter.setState('uebermorgen.boolean',   {ack: true, val: !!hd});
-    adapter.setState('aftertomorrow.name',    {ack: true, val: hd});
-    adapter.setState('aftertomorrow.boolean', {ack: true, val: !!hd});
+    adapter.setState("uebermorgen.Name",      {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, "de")});
+    adapter.setState("uebermorgen.boolean",   {ack: true, val: !!hd});
+    adapter.setState("aftertomorrow.name",    {ack: true, val: hd});
+    adapter.setState("aftertomorrow.boolean", {ack: true, val: !!hd});
 
     // next holiday
     var duration = 0;
@@ -152,20 +134,38 @@ function checkHolidays() {
 
         if (hd) {
             var date = getDateFromYearsDay(day, year);
-            adapter.setState('naechster.Name', {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, 'de')});
-            adapter.setState('next.name',      {ack: true, val: hd});
+            adapter.setState("naechster.Name", {ack: true, val: getHoliday(day, isLeap, easter, advent4, year, "de")});
+            adapter.setState("next.name",      {ack: true, val: hd});
 
             var nextHoliday = adapter.formatDate(date);
-            adapter.log.info('Next holiday: ' + hd + ' is in ' + duration + ' days on ' + nextHoliday);
+            adapter.log.info("Next holiday: " + hd + " is in " + duration + " days on " + nextHoliday);
 
-            adapter.setState('naechster.Datum', nextHoliday, true);
-            adapter.setState('next.date',       nextHoliday, true);
+            adapter.setState("naechster.Datum", nextHoliday, true);
+            adapter.setState("next.date",       nextHoliday, true);
 
-            adapter.setState('naechster.Dauer', duration, true);
-            adapter.setState('next.duration',   duration, true, function () {
+            adapter.setState("naechster.Dauer", duration, true);
+            adapter.setState("next.duration",   duration, true, function () {
                 adapter.stop();
             });
             break;
         }
     } while (!hd);
 }
+
+adapter.on("ready", function () {
+    adapter.getForeignObject("system.config", function (err, data) {
+        if (data && data.common) {
+            lang = data.common.language;
+        }
+
+        adapter.log.debug("adapter feiertage initializing objects");
+        checkHolidays();
+        adapter.log.info("adapter feiertage objects written");
+
+        setTimeout(function () {
+            adapter.log.info("force terminating after 1 minute");
+            adapter.stop();
+        }, 60000);
+
+    });
+});
